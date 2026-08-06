5fdb404e50b2ba0719cbb7058aacb58b8009b884
NagasriHarini <hariniraj8899@gmail.com>
2026-08-06 12:17:55 +0530

    Registering_student_database_connected

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-
-
+from database import register_student
 app = Flask(__name__)
 
 
@@ -16,8 +15,15 @@ def student():
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+
+    if request.method == 'POST':
+
+        data = request.form
+
+        register_student(data)
+        return 'successfully registered'
     return render_template('register.html')
 
 @app.route('/Admin')
--- database.py
+++ database.py
@@ -1,10 +1,37 @@
 import pyodbc
+from werkzeug.security import generate_password_hash
 
-conn = pyodbc.connect(
+connection_string = (
     "DRIVER={ODBC Driver 17 for SQL Server};"
     "SERVER=localhost\\SQLEXPRESS;"
     "DATABASE=course_registration_system;"
     "Trusted_Connection=yes;"
 )
 
-cursor  = conn.cursor()
+def register_student(data):
+
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    hashed_password = generate_password_hash(data["password"])
+
+    cursor.execute("""
+        INSERT INTO Students
+        (student_id, first_name, last_name, email,
+         password, department, year_of_study, phone_number,section)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (
+        data["student_id"],
+        data["first_name"],
+        data["last_name"],
+        data["email"],
+        hashed_password,
+        data["department"],
+        data["year_of_study"],
+        data["phone_number"],
+        data["section"]
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
